5f6d89c1b7edead2e7898504d4fd50d1d4634a78
Ilja Stepanow <ghyston@gmail.com>
2022-05-23 22:29:49 +0200

    Use separate CircleService class

--- client/src/CirclesField.tsx
+++ client/src/CirclesField.tsx
@@ -1,57 +1,21 @@
-import { HubConnection, HubConnectionBuilder, HubConnectionState } from "@microsoft/signalr";
-import axios from "axios";
 import * as d3 from "d3";
 import { observer } from "mobx-react-lite";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import CircleDto from "./CircleDto";
+import { circlesService } from "./CirclesService";
 import { circlesStore } from "./CirclesStore";
 
 const CirclesField = observer(() => {
 
-    const createConnection = () => {
-        const connect = new HubConnectionBuilder()
-            .withUrl('https://localhost:7181/hub/circle')
-            .withAutomaticReconnect()
-            .build();
-
-        connect
-            .start()
-
-        return connect;
-    }
-
     const svgRef = useRef(null);
-    const [connection] = useState<HubConnection>(createConnection);
 
-    // Subscribe to new circles
     useEffect(() => {
-        connection.off("CircleCreated");
-        connection.on("CircleCreated", (dto: CircleDto) => {
-            circlesStore.addCircle(dto)
-        })
-
-        connection.off("CircleRemoved");
-        connection.on("CircleRemoved", (id: number) => {
-            circlesStore.removeCircle(id)
-        })
-
-        connection.send('SubscribeToColor', "red");
-
-    }, [connection])
-
-    // retrieve initial set of circles
-    useEffect(() => {
-        axios.get<CircleDto[]>(
-            'https://localhost:7181/circles',
-            {
-                headers: {
-                    Accept: 'application/json',
-                },
-            }
-        )
-            .then((response) => {
-                circlesStore.updateCircles(response.data)
-            })
+        circlesService.initialLoad();
+        circlesService.connect();
+        
+        return () => {
+            circlesService.disconnect()
+        }
     }, [])
 
     const drawCircles = (circles: CircleDto[]) => {
@@ -71,7 +35,7 @@ const CirclesField = observer(() => {
             .attr("fill", (dto: CircleDto) => dto.color)
             .on("click", async (e: MouseEvent, dto: CircleDto) => {
                 e.stopPropagation();
-                await connection.send('RemoveCircle', dto.id);
+                circlesService.removeCircle(dto.id)
             })
             .transition()
             .ease(d3.easeBounceOut)
@@ -87,20 +51,6 @@ const CirclesField = observer(() => {
             .remove();
     }
 
-    const handleClick = async (x: number, y: number) => {
-        if (connection?.state !== HubConnectionState.Connected) {
-            alert("not connected!");
-            return;
-        }
-
-        await connection.send('CreateCircle', { x: x, y: y });
-    }
-
-    const handleChange = (color: string, isOn: string) => {
-        alert(`${color}: ${isOn}`);
-        //TODO
-    }
-
     drawCircles(circlesStore.circles);
 
     return (
@@ -109,7 +59,7 @@ const CirclesField = observer(() => {
                 ref={svgRef}
                 width={640}
                 height={480}
-                onClick={(e: React.MouseEvent) => handleClick(e.clientX, e.clientY)}>
+                onClick={(e: React.MouseEvent) => circlesService.createCircle(e.clientX, e.clientY)}>
             </svg>
             
         </>
---
+++ client/src/CirclesService.ts
@@ -0,0 +1,74 @@
+import { HubConnection, HubConnectionBuilder, HubConnectionState } from "@microsoft/signalr";
+import axios from "axios";
+import CircleDto from "./CircleDto";
+import { circlesStore } from "./CirclesStore";
+
+class CirclesService {
+
+    connection?: HubConnection;
+
+    async connect() {
+        this.createConnection();
+        await this.connection?.start();
+
+        this.connection?.send('SubscribeToColor', "red");
+
+        this.connection?.on("CircleCreated", (dto: CircleDto) => {
+            circlesStore.addCircle(dto)
+        });
+        this.connection?.on("CircleRemoved", (id: number) => {
+            circlesStore.removeCircle(id)
+        });
+    }
+
+    async disconnect() {
+        this.createConnection();
+        this.connection?.off("CircleCreated");
+        this.connection?.off("CircleRemoved");
+        await this.connection?.stop();
+    }
+
+    initialLoad() {
+        axios.get<CircleDto[]>(
+            'https://localhost:7181/circles',
+            {
+                headers: {
+                    Accept: 'application/json',
+                },
+            }
+        )
+            .then((response) => {
+                circlesStore.updateCircles(response.data)
+            })
+    }
+
+    async createCircle(x: number, y: number) {
+        if (this.connection?.state !== HubConnectionState.Connected) {
+            alert("not connected!");
+            return;
+        }
+
+        await this.connection?.send('CreateCircle', { x: x, y: y });
+    }
+
+    async removeCircle(id: number) {
+        if (this.connection?.state !== HubConnectionState.Connected) {
+            alert("not connected!");
+            return;
+        }
+
+        await this.connection?.send('RemoveCircle', id);
+    }
+
+    private createConnection() {
+        if (this.connection)
+            return;
+
+        this.connection = new HubConnectionBuilder()
+            .withUrl('https://localhost:7181/hub/circle')
+            .withAutomaticReconnect()
+            .build();
+    }
+}
+
+export let circlesService = new CirclesService();
